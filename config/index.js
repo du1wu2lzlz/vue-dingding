a08b44cab0ce0cec2c5d6a1d5f0750fb3b019a97
luke <625376618@qq.com>
2018-02-01 17:54:30 +0800

    端口调整

--- config/index.js
+++ config/index.js
@@ -14,7 +14,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 80, // 端口调整
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
